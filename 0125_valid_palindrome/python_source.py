fd94935b580bbe83fadb050c69f8b02853beae1a
Arthur Dysart <github@arthurdys.art>
2018-11-27 23:52:09 -0500

    update solution 125

--- 0125_valid_palindrome/python_source.py
+++ 0125_valid_palindrome/python_source.py
@@ -2,9 +2,9 @@
 """
 Leetcode - Valid palindrome
 https://leetcode.com/problems/valid-palindrome
-Two pointer (merging) solution
 
 Created on Sat Nov  3 15:47:03 2018
+Updated on Tue Nov 27 23:42:28 2018
 @author: Arthur Dysart
 """
 
@@ -14,43 +14,135 @@ import sys
 
 # FUNCTION DEFINITIONS
 class Solution:
+    """
+    Iteration over character indicies in string.
+
+    Time complexity: O(n)
+      - Amortized iterate over all characters in string
+    Space complexity: O(1)
+      - Update constant number of pointers
+    """
+
     def is_palindrome(self, s):
         """
-        Determines if alphanumeric characters in string "s" constitute a palindrome.
+        Determines whether alphanumeric characters comprise a valid palindrome.
 
-        :type s: str
+        :param str s: input string
+        :return: True if relevant string characters are valid palindrome
         :rtype: bool
         """
         if not s:
             return True
-        else:
-            # Initializes left start and end pointers
-            l = 0
-            r = len(s) - 1
-            while l < r:
-                if not s[l].isalnum():
-                    l += 1
-                elif not s[r].isalnum():
-                    r -= 1
-                elif s[l].upper() != s[r].upper():
-                    return False
-                else:
-                    l += 1
-                    r -= 1
+
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+
+            # Move pointers to next valid characters
+            l, r = self.eval_next_char(l, r, s)
+
+            if (l == len(s) - 1 and
+                r == 0):
+                # No valid characters found
+                return True
+
+            elif s[l].lower() != s[r].lower():
+                # Invalid palindrome
+                return False
+
+            else:
+                l += 1
+                r -= 1
+
+        # Valid palindrome
+        return True
+
+    def eval_next_char(self, l, r, s):
+        """
+        Moves index pointers to next alphanumeric character.
+
+        :param int l: pointer to left-most target index
+        :param int r: pointer to right-most target index
+        :param str s: input string
+        :return: updated index pointers
+        :rtype: tuple[int, int]
+        """
+        while (not s[l].isalnum() and
+               l < len(s) - 1):
+            l += 1
+
+        while (not s[r].isalnum() and
+               r > 0):
+            r -= 1
+
+        return l, r
+
+class Solution2:
+    """
+    Iteration over character indicies in string.
+
+    Time complexity: O(n)
+      - Amortized iterate over all characters in string
+    Space complexity: O(1)
+      - Update constant number of pointers
+    """
+
+    def is_palindrome(self, s):
+        """
+        Determines whether alphanumeric characters comprise a valid palindrome.
+
+        :param str s: input string
+        :return: True if relevant string characters are valid palindrome
+        :rtype: bool
+        """
+        if not s:
             return True
 
-def stdin(sys_stdin):
-    """
-    Imports standard input.
-    """
-    return [x for x in sys_stdin][0]
+        l = 0
+        r = len(s) - 1
+        
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            elif s[l].upper() != s[r].upper():
+                return False
+            else:
+                l += 1
+                r -= 1
+        return True
+
+class Input:
+
+    def stdin(self, sys_stdin):
+        """
+        Imports standard input.
+
+        :param _io.TextIOWrapper sys_stdin: standard input
+        :return: array of strings
+        :rtype: list[str]
+        """
+        inputs = [x.strip("[]\"\n")
+                  for x
+                  in sys_stdin]
+
+        s = inputs[0]
+
+        return s
 
 
+## MAIN MODULE
 if __name__ == "__main__":
     # Imports standard input
-    s = stdin(sys.stdin)
-    
+    s = Input()\
+        .stdin(sys.stdin)
+
     # Evaluates solution
-    r = Solution()\
+    z = Solution()\
         .is_palindrome(s)
-    print(r)
+    print(z)
+
+
+## END OF FILE
